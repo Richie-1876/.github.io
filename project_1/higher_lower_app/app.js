5be667be3689952dcdc8d59376f3f33348d2fded
Richard Wright <richardwright@Richards-MacBook-Pro.local>
2020-02-08 16:38:45 +0000

    sucessful api call image rendered to the game screen with jquery

--- project_1/higher_lower_app/app.js
+++ project_1/higher_lower_app/app.js
@@ -1,2 +1,13 @@
-console.log('connected');
-console.log($);
+// console.log('connected');
+// console.log($);
+
+$(() => {
+  $.ajax({
+    url: 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
+  }).then((data) => {
+    console.log(data);
+    let $img = $('<img>').addClass('card').attr('src', data.cards[0].image)
+    $img.appendTo('.game-screen')
+
+  })
+}) //onload ends here
